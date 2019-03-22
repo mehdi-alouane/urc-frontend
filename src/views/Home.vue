59e8561e6851b75650f419299519de91df7a808d
mehdi-alouane <pro4medo@gmail.com>
2019-03-22 02:45:11 +0100

    ref: get and display nearby shops

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline">
+        <div class="column is-4" v-for="nearbyShop in nearbyShops" :key="nearbyShop.id">
+          <div class="box">
+            <p>{{ nearbyShop.name }} , {{ nearbyShop.city }}</p>
+            <figure class="image">
+              <img :src="nearbyShop.picture">
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from '@/services/axios'
 
 export default {
-  name: 'home'
+  name: 'home',
+  data: () => ({
+    coordinates: null,
+    nearbyShops: [],
+    page: 1
+  }),
+  methods: {
+    async getNearbyShops () {
+      const coordinates = await this.$getLocation()
+      this.coordinates = [coordinates.lat, coordinates.lng].join(',')
+
+      const api = `/shops/nearby/${this.coordinates}?page=${this.page}`
+      const { data } = await axios.get(api)
+
+      // console.log(data)
+      this.nearbyShops = data.nearByShops
+    }
+  },
+  created () {
+    this.getNearbyShops()
+  }
 }
 </script>
