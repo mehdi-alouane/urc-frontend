33fa3dfc8477b99033f07725c672dac94aee0d84
mehdi-alouane <pro4medo@gmail.com>
2019-03-22 02:43:23 +0100

    ref: add vueGeolocation to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGeolocation from 'vue-browser-geolocation'
+
+Vue.use(VueGeolocation)
 
 Vue.config.productionTip = false
 
