d9e81c8bc27b581d30a17adf25089432a9f039f8
mehdi-alouane <pro4medo@gmail.com>
2019-03-22 03:09:38 +0100

    ref: add shop card component

---
+++ src/components/shopCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <p>{{ shopName }} , {{ shopLocation }}</p>
+      <figure class="image">
+        <img :src="shopPicture">
+      </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shopCard',
+  props: {
+    shopName: String,
+    shopLocation: String,
+    shopPicture: String
+  }
+}
+</script>
