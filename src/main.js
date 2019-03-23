18e8a30bdcb07430667ad776fd91a835d5d2943f
mehdi-alouane <pro4medo@gmail.com>
2019-03-23 05:35:54 +0100

    ref: add axios to main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueGeolocation from 'vue-browser-geolocation'
+import axios from './services/axios'
 
 Vue.use(VueGeolocation)
+
+Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
 
