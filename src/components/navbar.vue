15ec8d697177c5a7d15110ef00f02c2c84cb1355
mehdi-alouane <pro4medo@gmail.com>
2019-03-24 05:39:08 +0100

    ref: add navbar component

---
+++ src/components/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar has-shadow">
+    <div class="container">
+      <div class="navbar-end">
+        <router-link
+          class="navbar-item"
+          :to="{ name: 'preferredShops' }"
+        >
+          preferred Shops
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
