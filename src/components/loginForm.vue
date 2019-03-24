95d4c73dfa0bec3b5efd58affe72da0d5aa9373a
mehdi-alouane <pro4medo@gmail.com>
2019-03-24 05:38:47 +0100

    ref: add login form

---
+++ src/components/loginForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="box">
+    <h1 class="title">
+      Please Login:
+    </h1>
+    <div class="field">
+      <div class="control">
+        <input
+          v-model="user.email"
+          type="text"
+          class="input"
+          placeholder="Email"
+        >
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <input
+          v-model="user.password"
+          type="password"
+          class="input"
+          placeholder="Password"
+        >
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <button
+          class="button
+          is-primary
+          is-fullwidth"
+          @click.prevent="loginUser"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data: () => ({
+    user: {
+      email: null,
+      password: null
+    }
+  }),
+  methods: {
+    loginUser () {
+      this.$store.dispatch('setToken', this.user)
+      this.$router.push({ name: 'home' })
+    }
+  }
+}
+</script>
