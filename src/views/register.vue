dfd0376e85a721ab2eec475dd4c3df0c25b74e1f
mehdi-alouane <pro4medo@gmail.com>
2019-03-24 05:40:44 +0100

    ref: add register-form to register view

---
+++ src/views/register.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-5">
+        <register-form />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import registerForm from '@/components/registerForm'
+
+export default {
+  name: 'Register',
+  components: {
+    registerForm
+  }
+}
+</script>
