2670682170b1e1ec427ff552503deb6a61555918
mehdi-alouane <pro4medo@gmail.com>
2019-03-24 05:40:24 +0100

    ref: add login-form to login view

---
+++ src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-7">
+        <login-form />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import loginForm from '@/components/loginForm'
+
+export default {
+  components: {
+    loginForm
+  }
+}
+</script>
