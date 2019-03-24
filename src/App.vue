8c607c09f8ca682ad73e296bd8fab30b3093fa82
mehdi-alouane <pro4medo@gmail.com>
2019-03-24 05:36:51 +0100

    ref: add font awesome to App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="sass" src="bulma">
+</style>
+
+<style lang="scss" src="../node_modules/font-awesome/scss/font-awesome.scss">
 </style>
